332a012e8b2db070a40dd7d86eb341c39fa220d0
liz <lissete.trejo@gmail.com>
2023-07-26 02:14:26 -0700

    creates basic Wallet routes (get all, get one, create, update, delete)

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router()
+const { 
+  getAll, 
+  getItem, 
+  getOne, 
+  createItem, 
+  updateItem, 
+  deleteItem
+} = require('./routeHelpers');
 
 const Wallet = require('../models/Wallet');
 
-// Example route: GET /
-router.get('/', (req, res) => {
-  res.json("success");
-});
+// GET all Wallets
+router.get('/', getAll(Wallet));
+
+// GET one Wallet
+router.get('/:id', getItem(Wallet), getOne(Wallet));
+
+// CREATE one Wallet
+router.post('/', createItem(Wallet));
+
+// UPDATE one Wallet
+const allowedUpdates = ['name', 'balance', 'type'];
+router.patch('/:id', getItem(Wallet), updateItem(Wallet, allowedUpdates)); 
+
+// DELETE a Wallet
+router.delete('/:id', getItem(Wallet), deleteItem(Wallet));
 
 module.exports = router;
