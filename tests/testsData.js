32ef49542460aedba94d1a6a58d87707762beed4
liz <lissete.trejo@gmail.com>
2023-07-26 02:15:09 -0700

    adds wallet test data

--- tests/testsData.js
+++ tests/testsData.js
@@ -12,7 +12,21 @@ const user2 = {
   password: 'testpassword2',
 };
 
+const wallet1 = {
+  name: 'Test Wallet',
+  balance: 1000,
+  type: 'checking',
+};
+
+const wallet2 = {
+  name: 'Test Wallet 2',
+  balance: 2000,
+  type: 'savings',
+};
+
 module.exports = {
   user1,
   user2,
+  wallet1,
+  wallet2,
 };
