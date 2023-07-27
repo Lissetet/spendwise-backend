e4e05f20c288eea4d7c88c76cf450c3fe6287f5a
liz <lissete.trejo@gmail.com>
2023-07-27 08:08:47 -0700

    edits UserSchema to define pre findOneAndDelete function to delete all associated Wallet, Budget, Category, and Transaction instances associated with the User.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const Wallet = require('./Wallet');
+const Budget = require('./Budget');
+const Category = require('./Category');
+const Transaction = require('./Transaction');
 
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -7,5 +11,19 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
 });
+
+UserSchema.pre('findOneAndDelete', async function(next) {
+  try {
+    const id = this._conditions._id;
+    await Wallet.deleteMany({ user: id });
+    await Budget.deleteMany({ user: id });
+    await Category.deleteMany({ users: { $all: [id] } });
+    await Transaction.deleteMany({ user: id });
+    next();
+  } catch (error) {
+    next(error);
+  }
+  next()
+})
 
 module.exports = mongoose.model('User', UserSchema);
